ae62de7bd9a6c08fbde6650b63d2d8aa10ae1163
Hemnath V <167060345+Hemnath-V@users.noreply.github.com>
2025-03-30 14:18:09 +0530

    Add files via upload

---
+++ day16_react_basics/App.jsx
@@ -0,0 +1,16 @@
+// import HelloWorld from "./react_learning/day16_react_intro/HelloWorld"
+// import Greeting from "./react_learning/day17_components/Greeting"
+// import Greeting from "./react_learning/day17_components/GreetingClass"
+
+import JsxExample from "./react_learning/day18_jsx/jsxExample"
+import PropsExample from "./react_learning/day19_props/PropsExample"
+
+function App() {
+  // return <HelloWorld />
+  // return <Greeting />
+  // return <Greeting />
+  // return <JsxExample />
+  return <PropsExample title="Hello" />
+}
+
+export default App
