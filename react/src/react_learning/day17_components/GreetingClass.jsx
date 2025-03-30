977c6079e1839d0399fbb6c0cefd89cc5ecb9b5c
heni-2802 <hemnathvrx@gmail.com>
2025-03-30 13:54:31 +0530

    Saved local changes before pulling

---
+++ react/src/react_learning/day17_components/GreetingClass.jsx
@@ -0,0 +1,9 @@
+// Class component
+
+import React from "react"
+class Greeting extends React.Component {
+    render() {
+        return <h1>Hello Hemnath</h1>
+    }
+}
+export default Greeting;
---
+++ react/src/react_learning/day19_props/PropsExample.jsx
@@ -0,0 +1,6 @@
+export default function PropsExample({ title }) {
+    return <>
+        <h1>Props Example</h1>
+        <p>hi {title}</p>
+    </>
+}
