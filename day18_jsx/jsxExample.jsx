a9b5a83a04639e8654f20fcfe86d83de434096b1
Hemnath V <167060345+Hemnath-V@users.noreply.github.com>
2025-04-02 12:58:27 +0530

    Add files via upload

---
+++ day18_jsx/jsxExample.jsx
@@ -0,0 +1,8 @@
+import "../../App.css"
+export default function jsxExample() {
+    const name = "Hemnath V"
+    return <>
+        <h1 className="bg-color">Hello World {name}</h1>
+        <p style={{ backgroundColor: "white", border: "10px solid black", fontSize: "50px", color: "black" }}>This is a sample</p>
+    </>
+}
